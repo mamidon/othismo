9d5af798cef4280ece7bec2128252481f624765c
mamidon <mamidon@liveaccount.co>
2024-01-23 22:20:10 -0800

    Rough in the CLI interface for creating a new image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct CliArguments {
+    #[command(subcommand)]
+    sub_command: Option<SubCommands>
+}
+#[derive(Subcommand)]
+enum SubCommands {
+    NewImage {
+        #[arg()]
+        image_name: String
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let command = CliArguments::parse();
+
+    if let Some(sub_command) = command.sub_command {
+        match sub_command {
+            SubCommands::NewImage { image_name } => println!("new-image {}", image_name)
+        }
+    }
 }
