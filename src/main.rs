8c1315ce4704e03fa1fe6e16e64d2684a2c58d21
mamidon <mamidon@liveaccount.co>
2024-01-26 08:49:28 -0800

    Rejigger module structure
    WIP roughing in module import

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,100 @@
 use clap::{Parser, Subcommand};
-use rusqlite::{Connection, OpenFlags, Result};
+
+mod solidarity {
+    use std::result;
+    use wasmer::CompileError;
+    use crate::solidarity;
+
+    #[derive(Debug)]
+    pub enum Error {
+        ImageAlreadyExists
+    }
+
+    pub type Result<T, E=Errors> = result::Result<T,E>;
+    #[derive(Debug)]
+    pub enum Errors {
+        Solidarity(Error),
+        Rusqlite(rusqlite::Error),
+        Io(std::io::Error),
+        Wasmer(wasmer::CompileError)
+    }
+
+    impl From<rusqlite::Error> for Errors {
+        fn from(value: rusqlite::Error) -> Self {
+            Errors::Rusqlite(value)
+        }
+    }
+
+    impl From<std::io::Error> for Errors {
+        fn from(value: std::io::Error) -> Self {
+            Errors::Io(value)
+        }
+    }
+
+    impl From<Error> for Errors {
+        fn from(value: solidarity::Error) -> Self {
+            Errors::Solidarity(value)
+        }
+    }
+
+    impl From<CompileError> for Errors {
+        fn from(value: CompileError) -> Self {
+            Errors::Wasmer(value)
+        }
+    }
+
+
+    pub mod image {
+        use std::path::{Path, PathBuf};
+        use rusqlite::{Connection, params};
+        use wasmer::{Module, Store};
+        use crate::solidarity::Error::ImageAlreadyExists;
+        use crate::solidarity::{Result, solidarity};
+
+        pub struct ImageFile {
+            path_name: PathBuf,
+            file: Connection,
+        }
+
+        impl ImageFile {
+            pub fn create<P: AsRef<Path>>(path: P) -> Result<ImageFile> {
+                if let Ok(_) = std::fs::metadata(&path) {
+                    Err(ImageAlreadyExists)?
+                }
+
+                let connection = Connection::open(path.as_ref())?;
+
+                connection.execute(include_str!("./sql_scripts/create_image_schema.sql"), ())?;
+
+                Ok(ImageFile {
+                    path_name: path.as_ref().to_path_buf(),
+                    file: connection
+                })
+            }
+
+            pub fn open<P: AsRef<Path>>(path: P) -> Result<ImageFile> {
+                Ok(ImageFile {
+                    path_name: path.as_ref().to_path_buf(),
+                    file: Connection::open(path)?
+                })
+            }
+
+            pub fn import_module<P: AsRef<Path>>(mut self, file_path: P, namespace_path: &str) -> Result<()> {
+                let wasm_bytes = std::fs::read(file_path.as_ref().canonicalize()?)?;
+
+                let mut store = Store::default();
+                Module::new(&store, &wasm_bytes)?;
+
+                self.file.execute("INSERT INTO module (path, wasm) VALUES (?,?)", params![
+                    namespace_path,
+                    wasm_bytes
+                ])?;
+
+                Ok(())
+            }
+        }
+    }
+}
 
 #[derive(Parser)]
 struct CliArguments {
@@ -38,7 +133,7 @@ enum SubCommands {
     SendMessage {},
 }
 
-fn main() -> Result<()> {
+fn main() -> solidarity::Result<()> {
     let command = CliArguments::parse();
 
     if let Some(image_name) = command.image_name {
@@ -79,15 +174,9 @@ fn main() -> Result<()> {
                 image_name
                  }) => {
                 let image_path = image_name.clone() + ".simg";
-                if let Ok(_) = std::fs::metadata(&image_path) {
-                    eprintln!("There is already an image file at {}", image_path);
-                    return Ok(());
-                }
+                let image = solidarity::image::ImageFile::create(image_path)?;
 
-                let connection = Connection::open(image_name.clone() + ".simg")?;
-
-                connection.execute(include_str!("./sql_scripts/create_image_schema.sql"), ())?;
-                println!("Image {} created", image_name);
+                println!("Image created");
             },
             _ => {
                 eprintln!("This sub-command needs the relevant image name specified before it");
