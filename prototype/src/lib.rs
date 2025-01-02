d119f3510c3c2d6821293cdfc0329ada55493fc6
mamidon <mamidon@outlook.com>
2025-01-01 16:03:52 -0800

    The beginnings of an ABI
    Every instance has an inbox & outbox buffer.  The host sends a message to the instance by resizing the inbox appropriatly with `prepare_inbox`, and writing to the inbox buffer directly.
    
    Then the host invokes `message_received`.  The client gets the correct buffer, based on the information provided earlier via `prepare_inbox`.
    
    The client sends a message to the host by preparing it's outbox buffer.  Since the outbox is a client side buffer, no need to invoke a method in the host.
    When the outbox buffer is ready, the client invokes `send_message` with the details of outbox buffers dimensions.

--- prototype/src/lib.rs
+++ prototype/src/lib.rs
@@ -1,25 +1,92 @@
-static mut BUFFER: [u8; 5] = *b"HELLO";
-static mut HEAP: Option<Box<String>> = None;
 
-#[no_mangle]
-pub extern "C" fn increment() -> i32 {
-    unsafe {
-        BUFFER = *b"NOOPE";
+#[allow(static_mut_refs)] // wasm is single threaded
+fn inbox() -> &'static mut MailBox {
+    static mut INBOX: Option<Box<MailBox>> = None;
+
+    unsafe { INBOX.get_or_insert(Box::new(MailBox::default())) }
+}
+
+#[allow(static_mut_refs)] // wasm is single threaded
+fn outbox() -> &'static mut MailBox {
+    static mut OUTBOX: Option<Box<MailBox>> = None;
+
+    unsafe { OUTBOX.get_or_insert(Box::new(MailBox::default())) }
+}
+
+struct MailBox {
+    head: *mut u8,
+    length: usize,
+    capacity: usize
+}
+
+impl MailBox {
+    pub fn as_slice(&self) -> &[u8] {
+        unsafe { std::slice::from_raw_parts(self.head, self.length) }
     }
 
-    let heap_ptr = unsafe {
-        match HEAP.as_mut() {
-            None => {
-                HEAP = Some(Box::new("START".to_string()));
-            },
-            Some(text) => {
-                text.push_str("NOPE");
-            }
-        };
+    pub fn as_mut_slice(&self) -> &mut [u8] {
+        unsafe { std::slice::from_raw_parts_mut(self.head, self.length) }
+    }
 
-        let s = HEAP.as_ref().unwrap();
-        s.as_ptr()
-    };
+    pub fn resize(&mut self, required_capacity: usize) {
+        self.length = required_capacity;
 
-    heap_ptr as i32
+        if self.capacity >= required_capacity {
+            return;
+        }
+
+        let mut buffer = unsafe { Vec::from_raw_parts(self.head, self.length, self.capacity) };
+        buffer.resize(required_capacity, 0u8);
+
+        self.head = buffer.as_mut_ptr();
+        std::mem::forget(buffer);
+    }
+}
+
+impl Default for MailBox {
+    fn default() -> Self {
+        let initial_capacity = 1024;
+        let mut buffer = Vec::with_capacity(initial_capacity);
+        let ptr = buffer.as_mut_ptr();
+        std::mem::forget(buffer);
+
+        Self {
+            head: ptr,
+            length: 0,
+            capacity: initial_capacity
+        }
+    }
+}
+
+impl Drop for MailBox {
+    fn drop(&mut self) {
+        let _ = unsafe { Vec::from_raw_parts(self.head, self.length, self.capacity) };
+    }
+}
+
+unsafe extern "C" {
+    unsafe fn send_message(bytes: *const u8, length: usize);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn prepare_inbox(message_length: usize) -> *mut u8 {
+    let inbox = inbox();
+
+    inbox.resize(message_length);
+
+    inbox.as_mut_slice().as_mut_ptr()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn message_received() {
+    let outbox = outbox();
+    let inbox = inbox();
+    
+    let message = inbox.as_slice();
+
+    outbox.resize(message.len());
+    outbox.as_mut_slice().copy_from_slice(message);
+
+    let outbox_slice = outbox.as_slice();
+    send_message(outbox_slice.as_ptr(), outbox_slice.len());
 }
