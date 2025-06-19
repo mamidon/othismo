c5b9a1f2cef319ea54437545f5da8dd9e05ae2e7
mamidon <mamidon@outlook.com>
2025-06-18 23:00:31 -0700

    Trying out zed...
    I can actually use it's git client lol

--- othismo/src/main.rs
+++ othismo/src/main.rs
@@ -3,12 +3,15 @@
 
 use std::time::Duration;
 
-use bson::{doc};
+use crate::othismo::{
+    execution,
+    image::{Image, Object},
+};
+use bson::doc;
 use clap::{Parser, Subcommand};
 use othismo::executors::{ConsoleExecutor, EchoExecutor};
 use othismo::namespace::Namespace;
 use tokio::time::sleep;
-use crate::othismo::{execution, image::{Image, Object}};
 
 mod othismo;
 
@@ -24,7 +27,7 @@ struct CliArguments {
 enum SubCommands {
     NewImage {
         #[arg()]
-        image_name: String
+        image_name: String,
     },
     ImportModule {
         #[arg()]
@@ -32,7 +35,7 @@ enum SubCommands {
     },
     RemoveModule {
         #[arg()]
-        module_name: String
+        module_name: String,
     },
     InstantiateInstance {
         #[arg()]
@@ -54,71 +57,67 @@ enum SubCommands {
 #[tokio::main]
 async fn main() -> othismo::Result<()> {
     let command = CliArguments::parse();
-    
+
     if let Some(image_name) = command.image_name {
         let mut image = Image::open(image_name.clone() + ".simg")?;
 
         match command.sub_command {
-            Some(SubCommands::ImportModule {
-                module_name
-            }) => {
+            Some(SubCommands::ImportModule { module_name }) => {
                 let module = std::fs::read(&module_name)?;
-                let module_namespace_name = &std::path::Path::new(&module_name).file_stem().unwrap().to_str().unwrap();
+                let module_namespace_name = &std::path::Path::new(&module_name)
+                    .file_stem()
+                    .unwrap()
+                    .to_str()
+                    .unwrap();
                 image.import_object(&module_namespace_name, Object::new_module(&module)?)?;
             }
-            Some(SubCommands::RemoveModule {
-                module_name
-            }) => {
+            Some(SubCommands::RemoveModule { module_name }) => {
                 image.remove_object(&module_name)?;
             }
             Some(SubCommands::InstantiateInstance {
                 module_name,
-                instance_name
+                instance_name,
             }) => {
                 let object = image.get_object(&module_name)?;
 
                 let module = match object {
                     Object::Instance(obj) => panic!("Please specify a module"),
-                    Object::Module(module) => module
+                    Object::Module(module) => module,
                 };
 
                 image.import_object(&instance_name, Object::Instance(module.into()))?;
             }
-            Some(SubCommands::DeleteInstance {
-                instance_name
-            }) => {
+            Some(SubCommands::DeleteInstance { instance_name }) => {
                 image.remove_object(&instance_name)?;
             }
-            Some(SubCommands::SendMessage {
-                instance_name
-            }) => {                
+            Some(SubCommands::SendMessage { instance_name }) => {
                 let mut namespace = Namespace::new();
                 namespace.create_process::<ConsoleExecutor>("/");
                 namespace.send_document("/", doc! { "hello": "world" });
+                namespace.send_document("/foo", doc! { "test": "zed" });
+
                 sleep(Duration::from_secs(10)).await;
-            },
-            Some(SubCommands::NewImage {
-                image_name: _
-            }) => {
+            }
+            Some(SubCommands::NewImage { image_name: _ }) => {
                 eprintln!("Specify the image name _after_ the new-image command");
-            },
-            Some(SubCommands::ListObjects {  }) => {
+            }
+            Some(SubCommands::ListObjects {}) => {
                 for name in image.list_objects("")? {
                     println!("{}", name);
                 }
             }
-            None => {eprintln!("No sub command specified");}
+            None => {
+                eprintln!("No sub command specified");
+            }
         }
     } else {
         match command.sub_command {
-            Some(SubCommands::NewImage {
-                image_name
-                 }) => {
+            Some(SubCommands::NewImage { image_name }) => {
                 let image_path = image_name.clone() + ".simg";
                 othismo::image::Image::create(image_path)?;
 
                 println!("Image created");
-            },
+            }
             _ => {
                 eprintln!("This sub-command needs the relevant image name specified before it");
             }
